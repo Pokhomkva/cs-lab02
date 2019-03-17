431c4d1eaa1765388f4da59b960f07921821cc26
Alice(PokhomovaES) <alice@example.com>
2019-03-17 15:41:42 +0300

    max для алисы

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
 using namespace std;
-//
+
 int main()
 { cout<<"Enter Aand B:";
-int a, b;
+int a, b, max;
 cin>>a>>b;
 cout <<"A+B=" << a+b<<'\n'
      <<"A-B="<<a-b<<'\n'
      <<"A*B=" << a*b<<'\n';
+if (a > b)
+a = max;
+else b = max;
 retutn (0);
 }
