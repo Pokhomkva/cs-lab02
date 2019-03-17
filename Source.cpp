51fb04957f35d97896be7b899387ea67262b67f7
Alice(PokhomovaES) <alice@example.com>
2019-03-17 15:24:27 +0300

    частное для алисы

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+
+# include  < iostream >
+
+using namespace std;
+//
+int  main()
+{
+	cout << " ¬ведите A и B: ";
+	int a, b;
+	cin >> a >> b;
+	cout << " A + B = " << a + b << ' \ n '
+		<< " AB = " << a*b << ' \ n '
+		<< " A * B = " << a * b << ' \ n ';
+	cout     << "A:B=" << a/b;
+	return(0);
+}
