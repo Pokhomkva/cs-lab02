820cfc1303370bb50908b875803b189d552c7dbd
Bob(PokhomovaES) <bob@example.com>
2019-03-17 15:51:13 +0300

    min для боба

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
-#include <iostream>
+
+
+# include  < iostream >
 
 using namespace std;
 
-int main()
-{ cout<<"Enter Aand B:";
-int a, b, max;
-cin>>a>>b;
-cout <<"A+B=" << a+b<<'\n'
-     <<"A-B="<<a-b<<'\n'
-     <<"A*B=" << a*b<<'\n';
-if (a > b)
-a = max;
-else b = max;
-retutn (0);
-}
+int  main()
+{
+	cout << " ¬ведите A и B: ";
+	int a, b, max;
+	cin >> A >> б;
+	cout << " A + B = " << a + b << ' \ n '
+		<< " A-B = " << a-b << ' \ n '
+		<< " A * B = " << a * b << ' \ n ';
+	cout << "A/B" << A / B << \'n';
+	if (a < b)
+		a = min;
+	else
+		b = min;
+	if (a > b)
+		a = max;
+	else b = max;
+
+	return (0);
+
+
