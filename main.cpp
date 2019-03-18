a34c47e0d884b915961d53f07091f2c3c583dc9a
Alice (KozlyukDA) <alice@example.com>
2019-03-18 09:31:55 +0300

    double

---
+++ main.cpp
@@ -0,0 +1,25 @@
+# include  < iostream >
+
+
+ using namespace std;
+
+
+ int main() {
+ double a, b, max;
+ cout<<"Enter A and B:";
+	cin>>a>>b;
+
+cout <<"A+B=" << a+b<<'\n'
+     <<"A-B="<<a-b<<'\n'
+     <<"A*B=" << a*b<<'\n';
+     cout << "A/B" << A / B << \'n';
+if (a > b)
+a = max;
+else b = max;		if (a < b)
+			a = min;
+		else
+		b = min;
+
+
+ 	return (0);
+ }
